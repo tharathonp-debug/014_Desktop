5befeabe96fe0e537f67e02c403d748e2eab3f70
tharathonp-debug <tharathon.p@rmutsvmail.com>
2025-12-30 10:07:04 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 				<h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนท์</font></center></h1>
 				<?php include "Date.php";?>
 			</td>
-			<td width="43" rowspan="2">&nbsp;</td>กกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกก
+			<td width="43" rowspan="2">&nbsp;</td>
 		</tr>
 		<tr>
 			<td width="1007" bgcolor="#FFFFFF">กกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกก
@@ -31,4 +31,5 @@
 		</tr>
 	</table>
 <body>
+
 
